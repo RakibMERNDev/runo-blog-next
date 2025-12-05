817d67bb8a5bcd2c71da69ee5e894ca96f63aae9
RakibMernDev <abdullahalrakib30@gmail.com>
2025-12-05 06:22:52 +0600

    added google fonts

---
+++ src/assets/fonts.ts
@@ -0,0 +1,13 @@
+import { Lora, League_Spartan, Roboto } from "next/font/google";
+
+export const LoraFont = Lora({
+  weight: ["400", "500", "600", "700"],
+});
+
+export const LeagueSpartanFont = League_Spartan({
+  weight: ["400", "500", "600", "700"],
+});
+
+export const RobotoFont = Roboto({
+  weight: ["400", "500", "700"],
+});
